76b8d600804e00d7df61ea6836b4b5147d0dde01
pbcong <congphamba2005@gmail.com>
2025-03-01 23:39:12 +0800

    fix api call bug

--- extension/background.js
+++ extension/background.js
@@ -81,7 +81,7 @@ async function detectDeepfake(imageUrl, tabId) {
         Accept: "application/json",
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ text: imageUrl }),
+      body: JSON.stringify({ image_url: imageUrl }),
     });
 
     if (!response.ok) {
